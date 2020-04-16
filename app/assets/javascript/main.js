f08c4fc041c0d94e9408289f7a372de0674453ae
Lydia Teebay <lydia@hippodigital.co.uk>
2020-04-16 14:55:47 +0100

    conditionally revealing new email

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,29 @@
 // ES6 or Vanilla JavaScript
+
+// conditional extra email
+var FormStuff = {
+  
+    init: function() {
+      this.applyConditionalRequired();
+      this.bindUIActions();
+    },
+    
+    bindUIActions: function() {
+      $("input[type='radio'], input[type='checkbox']").on("change", this.applyConditionalRequired);
+    },
+    
+    applyConditionalRequired: function() {
+      
+      $(".require-if-active").each(function() {
+        var el = $(this);
+        if ($(el.data("require-pair")).is(":checked")) {
+          el.prop("required", true);
+        } else {
+          el.prop("required", false);
+        }
+      });
+      
+    }
+    
+  };
+  
