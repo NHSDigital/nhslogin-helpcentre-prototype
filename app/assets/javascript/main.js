c797c3f92c81126b7f38705d86c9ad6c0ab01d57
Pelin Demir <pelin.demir@hippodigital.co.uk>
2023-09-07 21:26:43 +0100

    CSS and JS added to change side nav selected item to active state

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -26,4 +26,20 @@ var FormStuff = {
     }
     
   };
+
+
+// side nav changing active state everytime an item is selected
+
+document.querySelector(".article-section-nav__list").addEventListener("click", function(event) {
+  if (event.target.tagName === "A") {
+    // Remove the "active" class from all navigation items
+    const navItems = document.querySelectorAll(".article-section-nav__list a");
+    navItems.forEach(item => {
+      item.classList.remove("active");
+    });
+
+    // Add the "active" class to the clicked item
+    event.target.classList.add("active");
+  }
+});
   
